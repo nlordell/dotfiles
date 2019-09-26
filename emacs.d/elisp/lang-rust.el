98e7d6fb2bc347978b6cb74fc12b03f8fb206972
Nicholas Lordello <nlordell@gmail.com>
2019-09-26 16:25:02 +0200

    getting started with emacs again and emacs bootstrap

---
+++ emacs.d/elisp/lang-rust.el
@@ -0,0 +1,44 @@
+;; rust-mode, racer, cargo
+
+;; rust-mode
+;; https://github.com/rust-lang/rust-mode
+(use-package rust-mode
+  :bind ( :map rust-mode-map
+         (("C-c C-t" . racer-describe)))
+  :config
+  (progn
+    ;; add flycheck support for rust
+    ;; https://github.com/flycheck/flycheck-rust
+    (use-package flycheck-rust)
+
+    ;; cargo-mode for all the cargo related operations
+    ;; https://github.com/kwrooijen/cargo.el
+    (use-package cargo)
+
+    ;; racer-mode for getting IDE like features for rust-mode
+    ;; https://github.com/racer-rust/emacs-racer
+    (use-package racer
+      :config
+      (progn
+        ;; set racer rust source path environment variable
+        (setq racer-rust-src-path (getenv "RUST_SRC_PATH"))
+        (defun my-racer-mode-hook ()
+          (set (make-local-variable 'company-backends)
+               '((company-capf company-files))))
+
+        ;; enable company and eldoc minor modes in rust-mode
+        (add-hook 'racer-mode-hook 'company-mode)
+        (add-hook 'racer-mode-hook 'eldoc-mode)))
+
+    (add-hook 'rust-mode-hook 'flycheck-mode)
+    (add-hook 'flycheck-mode-hook 'flycheck-rust-setup)
+    (add-hook 'rust-mode-hook 'racer-mode)
+    (add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+    ;; format rust buffers on save using rustfmt
+    (add-hook 'before-save-hook
+              (lambda ()
+                (when (eq major-mode 'rust-mode)
+                  (rust-format-buffer))))))
+
+(provide 'lang-rust)
---
+++ emacs.d/init-generated.el
@@ -0,0 +1,15 @@
+;;; package --- Main init file
+;;; Commentary:
+;;; This is my init file
+
+;;; Code:
+
+(add-to-list 'load-path (concat user-emacs-directory "elisp"))
+
+(require 'base)
+(require 'base-theme)
+(require 'base-extensions)
+(require 'base-functions)
+(require 'base-global-keys)
+
+(require 'lang-rust)
