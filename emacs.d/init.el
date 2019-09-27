362c790801d61ea1731b93af365f507bb6c60448
Nicholas Lordello <nlordell@gmail.com>
2019-09-27 12:13:51 +0200

    many more emacs goodies

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,6 @@
-;;; package --- Init
+;;; init.el --- Emacs init file
 ;;; Commentary:
-;;; Personal init file mostly tailored towards Rust development
+;;; Personal init file for Evil Rust development with some goodies
 
 ;;; Locals
 (defun expand-data-file-name (name)
@@ -10,10 +10,20 @@
          (emacs-data-directory (expand-file-name "emacs" xdg-data-home)))
     (expand-file-name name emacs-data-directory)))
 
+;;; Packaging
+(setq
+ package-user-dir           (expand-data-file-name "elpa/")
+ package-archives           '(("melpa" . "https://stable.melpa.org/packages/")
+                              ("gnu"   . "https://elpa.gnu.org/packages/"))
+ package-archive-priorities '(("melpa" . 1)
+                              ("gnu"   . 0)))
+(package-initialize)
+
 ;;; Customization
 (prefer-coding-system 'utf-8)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
 (show-paren-mode 1)
 
 (setq-default
@@ -36,15 +46,7 @@
 (setq custom-file (expand-data-file-name "custom.el"))
 (load custom-file 'noerror)
 
-;;; Packaging
-(setq
- package-user-dir           (expand-data-file-name "elpa/")
- package-archives           '(("melpa" . "https://stable.melpa.org/packages/")
-                              ("gnu"   . "https://elpa.gnu.org/packages/"))
- package-archive-priorities '(("melpa" . 1)
-                              ("gnu"   . 0)))
-(package-initialize)
-
+;;; Packages
 (unless package-archive-contents
   (package-refresh-contents))
 
@@ -53,41 +55,62 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;;; Packages
+;; Interface
+(use-package all-the-icons)
+
 (use-package evil
+  :init
+  (setq evil-want-keybinding nil)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+  (use-package evil-collection
+    :config
+    (evil-collection-init)))
+
+(use-package magit
+  :bind ("C-x g" . 'magit-status))
+
+(use-package neotree
+  :bind ([f8] . 'neotree-toggle)
+  :init
+  (setq neo-theme 'icons))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package which-key
   :config
   (which-key-mode))
 
-;;; Theming
-(use-package doom-themes
+;; Theming
+(use-package solarized-theme
   :defer t)
+(use-package zenburn-theme
+  :defer t)
+
 (use-package circadian
-  :config
+  :init
   (setq
    calendar-latitude  52.5
    calendar-longitude 13.4
-   circadian-themes   '((:sunrise . doom-nord-light)
-                        (:sunset  . doom-one)))
+   circadian-themes   '((:sunrise . solarized-light)
+                        (:sunset  . zenburn)))
+  :config
   (circadian-setup))
 
+;; Miscellaneous
+(use-package so-long
+  :disabled ; seems to not be on ELPA yet
+  :config
+  (global-so-long-mode 1))
+
 ;; TODO:
-;; - all-the-icons
-;; - hide-mode-line
-;; - neotree
-;; - dtrt-indent
-;; - smartparens
-;; - so-long
 ;;;;
 ;; - rust-mode
 ;; - ivy
 ;;   - counsel
 ;;   - swipper
 ;;   - avy
-;; - magit
 ;; - org-*
 ;;   - org-agenda
 ;; - flycheck
@@ -95,6 +118,7 @@
 ;; - avy
 ;;;;
 ;; - fzf
+;; - projectile
 ;; - ripgrep
 ;; - mode line
 ;; - evil goodies
