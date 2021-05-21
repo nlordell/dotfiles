2fde45256fe1fc30dbbce6412108577621ab3e17
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-05-21 18:51:09 +0200

    takeoff

---
+++ config/nvim/lua/ft/rust.lua
@@ -0,0 +1,15 @@
+local utils = require("utils")
+
+require("which-key").register({
+	c = {
+		name = "cargo",
+		b = {"<cmd>!cargo build<cr>", "Build"},
+		c = {"<cmd>!cargo clippy<cr>", "Check"},
+		C = {"<cmd>!cargo clippy --workspace --all-features --all-targets<cr>", "Check Workspace"},
+		F = {"<cmd>!cargo fmt --all<cr>", "Format Workspace"},
+		t = {"<cmd>!cargo test<cr>", "Test"},
+		T = {"<cmd>!cargo test --workspace --all-features<cr>", "Test Workspace"},
+	}
+}, {prefix = "<leader>"})
+
+utils.bbopts {expandtab=true}
