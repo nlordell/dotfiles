51f825745589045d8b9df3549e60e4c9caa2171c
Nicholas Rodrigues Lordello <n@lordello.net>
2024-11-20 21:53:10 +0100

    Add Magit Configuration
    We also explored using `ligature.el` to setup ligature support on
    Linux, but ultimately abandoned it since the package is not available
    on GNU ELPA (and I don't want to pull in MELPA at the moment). I kept
    the ligature configuration around for Berkeley Mono for when I figure
    out what I want to do with the package.

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,10 +1,14 @@
 (load "~/.config/emacs/sanemacs.el" nil t)
 ;;(load "~/.config/emacs/gtd.el" nil t)
+;;(load "~/.config/emacs/ligatures.el" nil t)
 
 (use-package which-key
   :ensure t
   :config
   (which-key-mode))
+
+(use-package magit
+  :ensure t)
 
 ;;; TODO(nlordell):
 ;;; 1. Try out `meow` modal editing mode
@@ -14,9 +18,7 @@
 ;;;    <https://happihacking.com/blog/posts/2023/dev-containers-emacs/>
 ;;; 5. Investigate other potential sane Emacs defaults
 ;;;    <https://idiomdrottning.org/bad-emacs-defaults>
-;;; 6. Install and learn magit
-;;;    <https://www.youtube.com/watch?app=desktop&v=2-0OwGTt0dI>
-;;; 7. Matering Emacs <https://www.masteringemacs.org/>
+;;; 6. Matering Emacs <https://www.masteringemacs.org/>
 
 ;;; Enable ligatures on macOS
 (if (fboundp 'mac-auto-operator-composition-mode)
