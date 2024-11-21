08ae423371b8abcf0a4b66bed4ba56d51a93a8bf
Nicholas Rodrigues Lordello <n@lordello.net>
2024-11-21 22:45:27 +0100

    Setup Meow Modal Editing
    So far so good...

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,24 +1,30 @@
 (load "~/.config/emacs/sanemacs.el" nil t)
+(load "~/.config/emacs/meow.el" nil t)
 ;;(load "~/.config/emacs/gtd.el" nil t)
 ;;(load "~/.config/emacs/ligatures.el" nil t)
+
+(use-package magit
+  :ensure t)
+
+(use-package meow
+  :ensure t
+  :config
+  (meow-setup)
+  (meow-global-mode 1))
 
 (use-package which-key
   :ensure t
   :config
   (which-key-mode))
 
-(use-package magit
-  :ensure t)
-
 ;;; TODO(nlordell):
-;;; 1. Try out `meow` modal editing mode
-;;; 2. Use built-in tree-sitter support (v29)
-;;; 3. Simplify GTD.el to match what I do in Journal.md
-;;; 4. Emacs over devcontainers/docker with TRAMP
+;;; 1. Use built-in tree-sitter support (v29)
+;;; 2. Simplify GTD.el to match what I do in Journal.md
+;;; 3. Emacs over devcontainers/docker with TRAMP
 ;;;    <https://happihacking.com/blog/posts/2023/dev-containers-emacs/>
-;;; 5. Investigate other potential sane Emacs defaults
+;;; 4. Investigate other potential sane Emacs defaults
 ;;;    <https://idiomdrottning.org/bad-emacs-defaults>
-;;; 6. Matering Emacs <https://www.masteringemacs.org/>
+;;; 5. Matering Emacs <https://www.masteringemacs.org/>
 
 ;;; Enable ligatures on macOS
 (if (fboundp 'mac-auto-operator-composition-mode)
