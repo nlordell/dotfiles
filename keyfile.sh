29cac8f112c9fff801224d6b5717a01a23d98616
Nicholas Rodrigues Lordello <n@lordello.net>
2026-04-20 17:01:34 +0200

    Additional File Permissions and Flags
    Set some additional file permissions and flags for the generated
    key file (to make it less accessible to other users and prevent
    it from being included in backups).

--- keyfile.sh
+++ keyfile.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-mkdir -p "$HOME/.local/share/keepass"
+KEYFILE="$HOME/.local/share/keepass/trezor.key"
+
+mkdir -m 0700 -p "$(dirname "$KEYFILE")"
 
 # We use the SLIP-0016 master key as the key file for our KeePass database,
 # with a more appropriate key.
@@ -13,4 +15,14 @@ KEY='Unlock vault.kdbx?'
 VALUE='2d650551248d792eabf628f451200d7f51cb63e46aadcbb1038aacb05e8c8aee2d650551248d792eabf628f451200d7f51cb63e46aadcbb1038aacb05e8c8aee'
 
 "${TREZORCTL:-trezorctl}" encrypt-keyvalue --address "$ADDRESS" --prompt always "$KEY" "$VALUE" \
-	| xxd -r -p > "$HOME/.local/share/keepass/trezor.key"
+	| xxd -r -p > "$KEYFILE"
+
+# Restrict permissions on the file. We also set the `nodump` flag which some
+# software will respect and omit from backups (notably, `tarsnap` which I use).
+chmod 0400 "$KEYFILE"
+if command -v chattr &>/dev/null; then
+	chattr +d "$KEYFILE"
+fi
+if command -v chflags &>/dev/null; then
+	chflags nodump "$KEYFILE"
+fi
